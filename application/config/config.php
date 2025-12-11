a750f69f26a0d1786f57f0752929c62bdbc7c63c
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-12-11 16:46:19 +0530

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $root=(isset($_SERVER["HTTPS"]) ? "https://" : "http://").$_SERVER["HTTP_HOST"];
 // $root.= str_replace(basename($_SERVER["SCRIPT_NAME"]), "", $_SERVER["SCRIPT_NAME"]);
 // $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https://" : "http://"). $_SERVER['HTTP_HOST'] . "/"; 
-$config['base_url'] = 'https://hrms-accrosian.up.railway.app';
+$config['base_url'] = 'https://hrms-accrosian.up.railway.app/';
 
 /*
 |--------------------------------------------------------------------------
