b963345f755bbaec8c4bebc348235aad36e64333
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-12-11 12:36:53 +0530

    Enable mysqli extension

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter/framework',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8ea3365088b642c7801ad62276033df413827986',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter/framework' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8ea3365088b642c7801ad62276033df413827986',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
