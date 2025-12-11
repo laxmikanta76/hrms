0df6deb3fa267ccc133c27e887b94474aa5a82b7
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-12-11 16:06:07 +0530

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,8 @@ $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 $root=(isset($_SERVER["HTTPS"]) ? "https://" : "http://").$_SERVER["HTTP_HOST"];
 $root.= str_replace(basename($_SERVER["SCRIPT_NAME"]), "", $_SERVER["SCRIPT_NAME"]);
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https://" : "http://"). $_SERVER['HTTP_HOST'] . "/"; 
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https://" : "http://")
+                      . $_SERVER['HTTP_HOST'] . "/";
 
 
 /*
