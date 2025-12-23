79f8fb02760a4959646b7974edfc11e312d5e1c0
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-12-23 15:44:37 +0530

    initial change

--- application/modules/attendance/views/attendance_log_userdetails.php
+++ application/modules/attendance/views/attendance_log_userdetails.php
@@ -80,17 +80,14 @@ $att_in = $this->db->select('a.*,b.first_name,b.last_name')
                                     <?php } ?>
                                 </td>
                                 <td>
-                                    <?php if (can_manage_attendance()): ?>
-                                    <a href="<?= base_url("attendance/home/delete_attendance/{$attendancedata->atten_his_id}/{$attendancedata->uid}") ?>"
+                                    <?php if($this->permission->method('atn_log_datewise','delete')->access()): ?>
+                                    <a href="<?php echo base_url("attendance/home/delete_attendance/$attendancedata->atten_his_id/$attendancedata->uid") ?>"
                                         onclick="return confirm('Are You Sure To Want to Delete?')"
-                                        class="btn btn-danger btn-sm">
-                                        <i class="fa fa-trash"></i>
-                                    </a>
-
-                                    <a href="<?= base_url("attendance/home/index/{$attendancedata->atten_his_id}") ?>"
-                                        class="btn btn-info btn-sm">
-                                        <i class="fa fa-edit"></i>
-                                    </a>
+                                        class="btn btn-danger"><i class="fa fa-close"></i></a>
+                                    <?php endif; ?>
+                                    <?php if($this->permission->method('atn_log_datewise','update')->access()): ?>
+                                    <a href="<?php echo base_url("attendance/home/index/$attendancedata->atten_his_id") ?>"
+                                        class="btn btn-info"><i class="fa fa-edit"></i></a>
                                     <?php endif; ?>
                                 </td>
                             </tr>
