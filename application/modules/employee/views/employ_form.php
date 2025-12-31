068341b8eab1fb8e0f38af8193a8dd63748f7d93
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-12-31 15:42:08 +0530

    initial change

--- application/modules/employee/views/employ_form.php
+++ application/modules/employee/views/employ_form.php
@@ -790,9 +790,10 @@ ul li a {
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="h_phone"><?php echo display('home_phone')?></label>
+                                            <label for="h_phone"><?php echo display('home_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="h_phone" name="h_phone"
-                                                placeholder="<?php echo display('home_phone')?>" autocomplete="off">
+                                                placeholder="<?php echo display('home_phone')?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
@@ -804,7 +805,8 @@ ul li a {
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="c_phone"><?php echo display('cell_phone')?></label>
+                                            <label for="c_phone"><?php echo display('cell_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="c_phone" name="c_phone"
                                                 placeholder="<?php echo display('cell_phone')?>">
                                         </div>
@@ -834,24 +836,27 @@ ul li a {
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="s_name"><?php echo display('emerg_contct')?>
+                                            <label for="s_name"><?php echo display('emerg_contct')?> <sup
+                                                    class="color-red ">*</sup></label>
 
-                                                <input type="text" class="form-control" id="em_contact"
-                                                    name="em_contact" placeholder="Emergency Contact">
+                                            <input type="text" class="form-control" id="em_contact" name="em_contact"
+                                                placeholder="Emergency Contact">
 
                                         </div>
                                     </div>
 
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="e_h_phone"><?php echo display('emerg_home_phone')?></label>
+                                            <label for="e_h_phone"><?php echo display('emerg_home_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="e_h_phone" name="e_h_phone"
                                                 placeholder="<?php echo display('emerg_home_phone')?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="e_w_phone"><?php echo display('emrg_w_phone')?></label>
+                                            <label for="e_w_phone"><?php echo display('emrg_w_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="e_w_phone" name="e_w_phone"
                                                 placeholder="<?php echo display('emrg_w_phone')?>">
                                         </div>
@@ -1333,121 +1338,70 @@ function valid_inf5() {
 
 }
 
-// function valid_inf6() {
-
-//     var h_phoneInput = document.getElementById('h_phone');
-//     var c_phoneInput = document.getElementById('c_phone');
-//     var h_phone = $('#h_phone').val();
-//     var c_phone = $('#c_phone').val();
-//     if (h_phone == "") {
-//         document.getElementById("h_phone").style.borderColor = "red";
-//     } else {
-//         $("#h_phone").on('keyup', function() {
-//             document.getElementById("h_phone").style.borderColor = "green";
-//         });
-
-//     }
-//     if (c_phone == "") {
-//         document.getElementById("c_phone").style.borderColor = "red";
-//     } else {
-//         $("#c_phone").on('keyup', function() {
-//             document.getElementById("c_phone").style.borderColor = "green";
-//         });
-
-//     }
-//     if (h_phone !== "" && c_phone !== "") {
-//         $('.nav-tabs > .active').next('li').find('a').trigger('click');
-//     }
-
-// }
 function valid_inf6() {
 
-    var h_phone = $('#h_phone').val().trim();
-    var c_phone = $('#c_phone').val().trim();
+    var h_phoneInput = document.getElementById('h_phone');
+    var c_phoneInput = document.getElementById('c_phone');
+    var h_phone = $('#h_phone').val();
+    var c_phone = $('#c_phone').val();
+    if (h_phone == "") {
+        document.getElementById("h_phone").style.borderColor = "red";
+    } else {
+        $("#h_phone").on('keyup', function() {
+            document.getElementById("h_phone").style.borderColor = "green";
+        });
 
-    // Reset borders
-    $('#h_phone, #c_phone').css('border-color', '');
+    }
+    if (c_phone == "") {
+        document.getElementById("c_phone").style.borderColor = "red";
+    } else {
+        $("#c_phone").on('keyup', function() {
+            document.getElementById("c_phone").style.borderColor = "green";
+        });
 
-    // If user typed something, validate length only
-    if (h_phone !== "" && h_phone.length < 10) {
-        $('#h_phone').css('border-color', 'red');
-        return false;
+    }
+    if (h_phone !== "" && c_phone !== "") {
+        $('.nav-tabs > .active').next('li').find('a').trigger('click');
     }
 
-    if (c_phone !== "" && c_phone.length < 10) {
-        $('#c_phone').css('border-color', 'red');
-        return false;
-    }
-
-    // Move to next tab ALWAYS
-    $('.nav-tabs > .active').next('li').find('a').trigger('click');
 }
 
-
-// function valid_inf7() {
-//     var em_contactInput = document.getElementById('em_contact');
-//     var em_contact = $('#em_contact').val();
-//     var e_h_phoneInput = document.getElementById('e_h_phone');
-//     var e_h_phone = $('#e_h_phone').val();
-//     var e_w_phoneInput = document.getElementById('e_w_phone');
-//     var e_w_phone = $('#e_w_phone').val();
-//     if (em_contact == "") {
-//         document.getElementById("em_contact").style.borderColor = "red";
-//     } else {
-//         $("#em_contact").on('keyup', function() {
-//             document.getElementById("em_contact").style.borderColor = "green";
-//         });
-
-//     }
-//     if (e_h_phone == "") {
-//         document.getElementById("e_h_phone").style.borderColor = "red";
-//     } else {
-//         $("#e_h_phone").on('keyup', function() {
-//             document.getElementById("e_h_phone").style.borderColor = "green";
-//         });
-
-//     }
-//     if (e_w_phone == "") {
-//         document.getElementById("e_w_phone").style.borderColor = "red";
-//     } else {
-//         $("#e_w_phone").on('keyup', function() {
-//             document.getElementById("e_w_phone").style.borderColor = "green";
-//         });
-
-//     }
-//     if (em_contact !== "" && e_h_phone !== "" && e_w_phone !== "") {
-//         $('.nav-tabs > .active').next('li').find('a').trigger('click');
-//     }
-
-// }
 function valid_inf7() {
+    var em_contactInput = document.getElementById('em_contact');
+    var em_contact = $('#em_contact').val();
+    var e_h_phoneInput = document.getElementById('e_h_phone');
+    var e_h_phone = $('#e_h_phone').val();
+    var e_w_phoneInput = document.getElementById('e_w_phone');
+    var e_w_phone = $('#e_w_phone').val();
+    if (em_contact == "") {
+        document.getElementById("em_contact").style.borderColor = "red";
+    } else {
+        $("#em_contact").on('keyup', function() {
+            document.getElementById("em_contact").style.borderColor = "green";
+        });
 
-    var em_contact = $('#em_contact').val().trim();
-    var e_h_phone = $('#e_h_phone').val().trim();
-    var e_w_phone = $('#e_w_phone').val().trim();
+    }
+    if (e_h_phone == "") {
+        document.getElementById("e_h_phone").style.borderColor = "red";
+    } else {
+        $("#e_h_phone").on('keyup', function() {
+            document.getElementById("e_h_phone").style.borderColor = "green";
+        });
 
-    $('#em_contact, #e_h_phone, #e_w_phone').css('border-color', '');
+    }
+    if (e_w_phone == "") {
+        document.getElementById("e_w_phone").style.borderColor = "red";
+    } else {
+        $("#e_w_phone").on('keyup', function() {
+            document.getElementById("e_w_phone").style.borderColor = "green";
+        });
 
-    // Only em_contact is required
-    if (em_contact === "") {
-        $('#em_contact').css('border-color', 'red');
-        return false;
+    }
+    if (em_contact !== "" && e_h_phone !== "" && e_w_phone !== "") {
+        $('.nav-tabs > .active').next('li').find('a').trigger('click');
     }
 
-    // Phone numbers optional, validate only if entered
-    if (e_h_phone !== "" && e_h_phone.length < 10) {
-        $('#e_h_phone').css('border-color', 'red');
-        return false;
-    }
-
-    if (e_w_phone !== "" && e_w_phone.length < 10) {
-        $('#e_w_phone').css('border-color', 'red');
-        return false;
-    }
-
-    $('.nav-tabs > .active').next('li').find('a').trigger('click');
 }
-
 
 function valid_inf8() {
 
--- application/modules/employee/views/update_employee_form.php
+++ application/modules/employee/views/update_employee_form.php
@@ -925,7 +925,8 @@ ul li a {
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="h_phone"><?php echo display('home_phone')?></label>
+                                            <label for="h_phone"><?php echo display('home_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="h_phone" name="h_phone"
                                                 placeholder="Home Phone" value="<?php echo $data->home_phone;?>">
 
@@ -940,7 +941,8 @@ ul li a {
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="c_phone"><?php echo display('cell_phone')?></label>
+                                            <label for="c_phone"><?php echo display('cell_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
 
                                             <input type="text" class="form-control" id="c_phone" name="c_phone"
                                                 placeholder="Cell Phone" value="<?php echo $data->cell_phone;?>">
@@ -972,7 +974,7 @@ ul li a {
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="s_name"><?php echo display('emerg_contct')?><sup
+                                            <label for="s_name"><?php echo display('emerg_contct')?> <sup
                                                     class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="em_contact" name="em_contact"
                                                 placeholder="Emergency Contact"
@@ -983,7 +985,8 @@ ul li a {
 
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="e_h_phone"><?php echo display('emerg_home_phone')?></label>
+                                            <label for="e_h_phone"><?php echo display('emerg_home_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
 
                                             <input type="text" class="form-control" id="e_h_phone" name="e_h_phone"
                                                 placeholder="Emergency Home Phone"
@@ -993,7 +996,8 @@ ul li a {
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="e_w_phone"><?php echo display('emrg_w_phone')?></label>
+                                            <label for="e_w_phone"><?php echo display('emrg_w_phone')?> <sup
+                                                    class="color-red ">*</sup></label>
                                             <input type="text" class="form-control" id="e_w_phone" name="e_w_phone"
                                                 placeholder="Emergency Work Phone"
                                                 value="<?php echo $data->emrg_w_phone;?>">
@@ -1205,7 +1209,7 @@ ul li a {
 
                                 <div class="form-group text-right">
                                     <input type="button" class="btn btn-primary btnPrevious" value="Previous">
-                                    <input type="submit" class="btn btn-success" value="Update">
+                                    <input type="submit" class="btn btn-success" onclick="valid_inf8()" value="Update">
                                 </div>
 
                                 <?php echo form_close() ?>
@@ -1449,7 +1453,6 @@ function valid_inf2() {
     }
     if (division !== "" && designation !== "" && hiredate !== "" && ohiredate !== "" && rate_type !== "" && rate !==
         "" && pay_frequency !== "") {
-
         $('.nav-tabs > .active').next('li').find('a').trigger('click');
     }
 }
@@ -1516,120 +1519,71 @@ function valid_inf5() {
     }
 
 }
-// function valid_inf6() {
 
-//   var h_phoneInput = document.getElementById('h_phone');
-//   var c_phoneInput = document.getElementById('c_phone');
-//   var h_phone = $('#h_phone').val();
-//   var c_phone = $('#c_phone').val();
-//   if (h_phone == "") {
-//     document.getElementById("h_phone").style.borderColor = "red";
-//   }else{
-//     $("#h_phone").on('keyup', function(){
-//      document.getElementById("h_phone").style.borderColor = "green";
-//    });
-
-//   }
-//   if (c_phone == "") {
-//   document.getElementById("c_phone").style.borderColor = "red";
-//   }else{
-//     $("#c_phone").on('keyup', function(){
-//      document.getElementById("c_phone").style.borderColor = "green";
-//    });
-
-//   }
-//   if(h_phone !== "" && c_phone !== ""){
-//    $('.nav-tabs > .active').next('li').find('a').trigger('click');
-//  }
-
-// }
 function valid_inf6() {
 
-    var h_phone = $('#h_phone').val().trim();
-    var c_phone = $('#c_phone').val().trim();
+    var h_phoneInput = document.getElementById('h_phone');
+    var c_phoneInput = document.getElementById('c_phone');
+    var h_phone = $('#h_phone').val();
+    var c_phone = $('#c_phone').val();
+    if (h_phone == "") {
+        document.getElementById("h_phone").style.borderColor = "red";
+    } else {
+        $("#h_phone").on('keyup', function() {
+            document.getElementById("h_phone").style.borderColor = "green";
+        });
 
-    // Reset borders
-    $('#h_phone, #c_phone').css('border-color', '');
+    }
+    if (c_phone == "") {
+        document.getElementById("c_phone").style.borderColor = "red";
+    } else {
+        $("#c_phone").on('keyup', function() {
+            document.getElementById("c_phone").style.borderColor = "green";
+        });
 
-    // If user typed something, validate length only
-    if (h_phone !== "" && h_phone.length < 10) {
-        $('#h_phone').css('border-color', 'red');
-        return false;
+    }
+    if (h_phone !== "" && c_phone !== "") {
+        $('.nav-tabs > .active').next('li').find('a').trigger('click');
     }
 
-    if (c_phone !== "" && c_phone.length < 10) {
-        $('#c_phone').css('border-color', 'red');
-        return false;
-    }
-
-    // Move to next tab ALWAYS
-    $('.nav-tabs > .active').next('li').find('a').trigger('click');
 }
 
-// function valid_inf7() {
-//     var em_contactInput = document.getElementById('em_contact');
-//     var em_contact = $('#em_contact').val();
-//     var e_h_phoneInput = document.getElementById('e_h_phone');
-//     var e_h_phone = $('#e_h_phone').val();
-//     var e_w_phoneInput = document.getElementById('e_w_phone');
-//     var e_w_phone = $('#e_w_phone').val();
-//     if (em_contact == "") {
-//         document.getElementById("em_contact").style.borderColor = "red";
-//     } else {
-//         $("#em_contact").on('keyup', function() {
-//             document.getElementById("em_contact").style.borderColor = "green";
-//         });
-
-//     }
-//     if (e_h_phone == "") {
-//         document.getElementById("e_h_phone").style.borderColor = "red";
-//     } else {
-//         $("#e_h_phone").on('keyup', function() {
-//             document.getElementById("e_h_phone").style.borderColor = "green";
-//         });
-
-//     }
-//     if (e_w_phone == "") {
-//         document.getElementById("e_w_phone").style.borderColor = "red";
-//     } else {
-//         $("#e_w_phone").on('keyup', function() {
-//             document.getElementById("e_w_phone").style.borderColor = "green";
-//         });
-
-//     }
-//     if (em_contact !== "" && e_h_phone !== "" && e_w_phone !== "") {
-//         $('.nav-tabs > .active').next('li').find('a').trigger('click');
-//     }
-
-// }
 function valid_inf7() {
+    var em_contactInput = document.getElementById('em_contact');
+    var em_contact = $('#em_contact').val();
+    var e_h_phoneInput = document.getElementById('e_h_phone');
+    var e_h_phone = $('#e_h_phone').val();
+    var e_w_phoneInput = document.getElementById('e_w_phone');
+    var e_w_phone = $('#e_w_phone').val();
+    if (em_contact == "") {
+        document.getElementById("em_contact").style.borderColor = "red";
+    } else {
+        $("#em_contact").on('keyup', function() {
+            document.getElementById("em_contact").style.borderColor = "green";
+        });
 
-    var em_contact = $('#em_contact').val().trim();
-    var e_h_phone = $('#e_h_phone').val().trim();
-    var e_w_phone = $('#e_w_phone').val().trim();
+    }
+    if (e_h_phone == "") {
+        document.getElementById("e_h_phone").style.borderColor = "red";
+    } else {
+        $("#e_h_phone").on('keyup', function() {
+            document.getElementById("e_h_phone").style.borderColor = "green";
+        });
 
-    $('#em_contact, #e_h_phone, #e_w_phone').css('border-color', '');
+    }
+    if (e_w_phone == "") {
+        document.getElementById("e_w_phone").style.borderColor = "red";
+    } else {
+        $("#e_w_phone").on('keyup', function() {
+            document.getElementById("e_w_phone").style.borderColor = "green";
+        });
 
-    // Only em_contact is required
-    if (em_contact === "") {
-        $('#em_contact').css('border-color', 'red');
-        return false;
+    }
+    if (em_contact !== "" && e_h_phone !== "" && e_w_phone !== "") {
+        $('.nav-tabs > .active').next('li').find('a').trigger('click');
     }
 
-    // Phone numbers optional, validate only if entered
-    if (e_h_phone !== "" && e_h_phone.length < 10) {
-        $('#e_h_phone').css('border-color', 'red');
-        return false;
-    }
-
-    if (e_w_phone !== "" && e_w_phone.length < 10) {
-        $('#e_w_phone').css('border-color', 'red');
-        return false;
-    }
-
-    $('.nav-tabs > .active').next('li').find('a').trigger('click');
 }
-
 
 function valid_inf8() {
 
