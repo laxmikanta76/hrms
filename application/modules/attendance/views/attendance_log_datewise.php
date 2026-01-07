8e98343b1a2f3e0c6349b441a90445cbc8b6b6f5
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-01-07 11:45:56 +0530

    initial change

--- application/modules/attendance/views/attendance_log_datewise.php
+++ application/modules/attendance/views/attendance_log_datewise.php
@@ -47,76 +47,81 @@
         <div class="panel panel-default thumbnail">
             <div class="panel-body">
                 <?php
-            // $att_in = [];
-            // $i=1;
-foreach ($queryd as $attendance) {?>
-                <table width="100%" class="table table-striped table-bordered table-hover">
-                    <tr>
-                        <th colspan="6" class="text-center">Attendance History of
-                            <?php echo date( "F d, Y", strtotime($attendance->mydate));?></th>
-                    </tr>
-                    <tr>
-                        <th><?php echo display('sl')?></th>
-                        <th><?php echo display('employee_name')?></th>
-                        <th><?php echo display('in_time')?></th>
-                        <th><?php echo display('out_time')?></th>
-                        <th><?php echo display('Stay')?></th>
-                        <th><?php echo display('action')?></th>
-                    </tr>
-                    <?php
-           // GET attendance records for this date
-$this->db->select('MIN(a.time) as intime, MAX(a.time) as outtime, a.uid, b.first_name, b.last_name');
-$this->db->from('attendance_history a');
-$this->db->join('employee_history b', 'a.uid = b.employee_id', 'left');
-$this->db->like('a.time', date("Y-m-d", strtotime($attendance->mydate)), 'after');
-$this->db->group_by('a.uid');
-$this->db->order_by('a.uid', 'ASC');
+                // Group attendance records by date
+                $grouped_attendance = [];
+                foreach ($queryd as $attendance) {
+                    $date_key = date("Y-m-d", strtotime($attendance->mydate));
+                    if (!isset($grouped_attendance[$date_key])) {
+                        $grouped_attendance[$date_key] = $attendance;
+                    }
+                }
+                
+                // Now loop through unique dates only
+                foreach ($grouped_attendance as $date_key => $attendance) {
+                ?>
+                <table width="100%" class="table table-striped table-bordered table-hover" style="margin-bottom: 30px;">
+                    <thead>
+                        <tr>
+                            <th colspan="6" class="text-center">Attendance History of
+                                <?php echo date("F d, Y", strtotime($attendance->mydate));?></th>
+                        </tr>
+                        <tr>
+                            <th><?php echo display('sl')?></th>
+                            <th><?php echo display('employee_name')?></th>
+                            <th><?php echo display('in_time')?></th>
+                            <th><?php echo display('out_time')?></th>
+                            <th><?php echo display('Stay')?></th>
+                            <th><?php echo display('action')?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                    // GET attendance records for this date
+                    $this->db->select('MIN(a.time) as intime, MAX(a.time) as outtime, a.uid, b.first_name, b.last_name');
+                    $this->db->from('attendance_history a');
+                    $this->db->join('employee_history b', 'a.uid = b.employee_id', 'left');
+                    $this->db->like('a.time', date("Y-m-d", strtotime($attendance->mydate)), 'after');
+                    $this->db->group_by('a.uid');
+                    $this->db->order_by('a.uid', 'ASC');
 
-// 🔥 If employee (NOT admin), show only own attendance
-if (!can_select_employee()) {
-    $this->db->where('a.uid', $this->session->userdata('employee_id'));
-}
+                    // If employee (NOT admin), show only own attendance
+                    if (!can_select_employee()) {
+                        $this->db->where('a.uid', $this->session->userdata('employee_id'));
+                    }
 
-$att_in = $this->db->get()->result();
-// $att_in = $this->db->select('MIN(a.time) as intime,MAX(a.time) as outtime,a.uid,b.first_name,b.last_name')
-// ->from('attendance_history a')
-// ->join('employee_history b','a.uid = b.employee_id','left')
-// ->like('a.time',date( "Y-m-d", strtotime($attendance->mydate)),'after')
-// ->group_by('a.uid')
-// ->order_by('a.uid','ASC')
-// ->get()
-// ->result();
+                    $att_in = $this->db->get()->result();
 
-
-
-
-            $idx=1;
-           foreach ($att_in as $attendancedata) {
-            ?>
-                    <tr>
-                        <td><?php echo $idx ?></td>
-                        <td><a
-                                href="<?php echo base_url('attendance/home/user_attendanc_details/'.$attendancedata->uid)?>"><?php echo $attendancedata->first_name.' '.$attendancedata->last_name ?></a>
-                        </td>
-                        <td><?php echo date( "H:i:s", strtotime($attendancedata->intime)) ?></td>
-                        <td><?php echo date( "H:i:s", strtotime($attendancedata->outtime)) ?></td>
-                        <td><?php 
-                $date_a = new DateTime($attendancedata->outtime);
-                $date_b = new DateTime($attendancedata->intime);
-                $interval = date_diff($date_a,$date_b);
-
-            echo $interval->format('%h:%i:%s');?></td>
-                        <td>
-                            <a class="btn btn-info"
-                                href="<?php echo base_url('attendance/home/user_attendanc_details/'.$attendancedata->uid) ?>">
-                                <i class="fa fa-eye"></i> Details
-                            </a>
-                        </td>
-                    </tr>
-                    <?php
-         $idx++; }
-        
-            ?>
+                    $idx = 1;
+                    foreach ($att_in as $attendancedata) {
+                    ?>
+                        <tr>
+                            <td><?php echo $idx ?></td>
+                            <td>
+                                <a
+                                    href="<?php echo base_url('attendance/home/user_attendanc_details/'.$attendancedata->uid)?>">
+                                    <?php echo $attendancedata->first_name.' '.$attendancedata->last_name ?>
+                                </a>
+                            </td>
+                            <td><?php echo date("H:i:s", strtotime($attendancedata->intime)) ?></td>
+                            <td><?php echo date("H:i:s", strtotime($attendancedata->outtime)) ?></td>
+                            <td><?php 
+                                $date_a = new DateTime($attendancedata->outtime);
+                                $date_b = new DateTime($attendancedata->intime);
+                                $interval = date_diff($date_a,$date_b);
+                                echo $interval->format('%h:%i:%s');
+                            ?></td>
+                            <td>
+                                <a class="btn btn-info"
+                                    href="<?php echo base_url('attendance/home/user_attendanc_details/'.$attendancedata->uid) ?>">
+                                    <i class="fa fa-eye"></i> Details
+                                </a>
+                            </td>
+                        </tr>
+                        <?php
+                        $idx++;
+                    }
+                    ?>
+                    </tbody>
                 </table>
                 <?php } ?>
                 <?= $links ?>
@@ -132,4 +137,3 @@ $(function() {
     });
 });
 </script>
-<script></script>
