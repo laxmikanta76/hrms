ace41d9d344858558a81c9523a45c618d5ce3b71
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-01-27 23:50:44 +0530

    change in employee_salary_type

--- application/modules/attendance/models/Csv_model.php
+++ application/modules/attendance/models/Csv_model.php
@@ -369,17 +369,23 @@ public function company_info(){
 
     public function auto_checkout_missing_users()
 {
-    $today = date('Y-m-d');
+     $today = date('Y-m-d');
 
     $this->db->where('attendance_date', $today);
-    $this->db->where('checkout_time IS NULL', null, false);
     $this->db->where('checkin_time IS NOT NULL', null, false);
+
+    // 🔥 HANDLE ALL FORGOT CHECKOUT CASES
+    $this->db->group_start();
+        $this->db->where('checkout_time IS NULL', null, false);
+        $this->db->or_where('checkout_time', '00:00:00');
+        $this->db->or_where('checkout_time = checkin_time', null, false);
+    $this->db->group_end();
 
     return $this->db->update(
         'attendance_history',
         [
             'checkout_time' => '18:00:00',
-            'checkout_type' => 'auto' // optional (recommended)
+            'checkout_type' => 'auto'
         ]
     );
 }
