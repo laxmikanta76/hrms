a04dff1ebb3f7ae2357f8ebbc7fa6db7309539a5
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-01-27 18:29:47 +0530

    change in employee_salary_type

---
+++ application/controllers/corn_daily.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cron extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('attendance/Csv_model');
+    }
+
+    public function auto_checkout()
+    {
+        // 🔐 Simple security token
+        $token = $this->input->get('token');
+        if ($token !== 'SECURE123') {
+            show_error('Unauthorized access', 403);
+        }
+
+        $currentTime = date('H:i');
+
+        // ⏰ Run only after 8 PM
+        if ($currentTime >= '21:00') {
+
+            $this->Csv_model->auto_checkout_missing_users();
+            echo "Auto checkout completed at ".date('H:i:s');
+
+        } else {
+            echo "Cron triggered before 8 PM";
+        }
+    }
+}
