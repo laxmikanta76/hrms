dcbd56aa2532deabed18f281d6abfba597f5fe90
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-01-31 15:51:08 +0530

    initial commit

--- application/modules/leave/views/other_leave_application_form.php
+++ application/modules/leave/views/other_leave_application_form.php
@@ -247,6 +247,7 @@
 
 
 <script language="javascript">
+console.log('WEEKEND FROM PHP:', "<?php echo $weekend ?>");
 $(function() {
     $("#f").datepicker({
         dateFormat: 'yy-mm-dd'
@@ -271,7 +272,6 @@ $(function() {
 // Calculate approved days (excluding weekends)
 $(document).ready(function(e) {
     function calculation() {
-        console.log('WEEKEND FROM PHP:', "<?php echo $weekend ?>");
         var date1 = new Date($('.leave_aprv_strt_date').val());
         var date2 = new Date($('.leave_aprv_end_date').val());
         var from = new Date($('.leave_aprv_strt_date').val());
