20619c59c0f21b45f084287ba10aff0bfef012e5
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-02-02 14:53:50 +0530

    change in add LOP

--- application/modules/payroll/views/salarysetup_form.php
+++ application/modules/payroll/views/salarysetup_form.php
@@ -30,7 +30,8 @@
                                 <?= form_open('payroll/Payroll/create_s_setup') ?>
                                 <div class="form-group row">
                                     <label for="employee_id"
-                                        class="col-sm-3 col-form-label"><?php echo display('employee_name') ?> *</label>
+                                        class="col-sm-3 col-form-label"><?php echo display('employee_name') ?>
+                                        *</label>
                                     <div class="col-sm-9">
                                         <?php echo form_dropdown('employee_id',$employee,null,'class="form-control" id="employee_id" style="width:615px" onchange="employechange(this.value)"') ?>
                                     </div>
@@ -57,18 +58,28 @@
                                                     <th style="padding:10px"><?php echo display('basic')?></th>
                                                     <td><input type="text" id="basic" name="basic" class="form-control"
                                                             disabled=""></td>
+                                                    <td></td>
                                                 </tr>
                                                 <?php
                  $x=0;
                  foreach ($slname as $ab){
-        //echo ++$x;
                   ?>
                                                 <tr>
-                                                    <th style="padding:10px"><?php echo $ab->sal_name ;?>(%)</th>
+                                                    <th style="padding:10px"><?php echo $ab->sal_name ;?></th>
                                                     <td><input type="text"
                                                             name="amount[<?php echo $ab->salary_type_id; ?>]"
                                                             class="form-control addamount" onkeyup="summary()"
                                                             id="add_<?php echo $x;?>"></td>
+                                                    <td style="padding:10px">
+                                                        <select
+                                                            name="calculation_type[<?php echo $ab->salary_type_id; ?>]"
+                                                            class="form-control calc-type"
+                                                            id="calc_type_add_<?php echo $x;?>"
+                                                            onchange="handleCalculationType(this, 'add_<?php echo $x;?>')">
+                                                            <option value="0">%</option>
+                                                            <option value="1">Amount</option>
+                                                        </select>
+                                                    </td>
                                                 </tr>
                                                 <?php
                 $x++;}
@@ -84,21 +95,40 @@
                 foreach ($sldname as $row){
                   ?>
                                                 <tr>
-                                                    <th style="padding:10px"><?php echo $row->sal_name ;?> (%)</th>
+                                                    <th style="padding:10px"><?php echo $row->sal_name ;?></th>
                                                     <td><input type="text"
                                                             name="amount[<?php echo $row->salary_type_id; ?>]"
                                                             onkeyup="summary()" class="form-control deducamount"
                                                             id="dd_<?php echo $y;?>"></td>
+                                                    <td style="padding:10px">
+                                                        <select
+                                                            name="calculation_type[<?php echo $row->salary_type_id; ?>]"
+                                                            class="form-control calc-type"
+                                                            id="calc_type_dd_<?php echo $y;?>"
+                                                            onchange="handleCalculationType(this, 'dd_<?php echo $y;?>')">
+                                                            <option value="0">%</option>
+                                                            <option value="1">Amount</option>
+                                                        </select>
+                                                    </td>
                                                 </tr><?php
                $y++; }
                 ?>
                                                 <tr>
-                                                    <th style="padding:10px"><?php echo display('tax')?> (%)</th>
+                                                    <th style="padding:10px"><?php echo display('tax')?></th>
                                                     <td><input type="text" name="amount[]" onkeyup="summary()"
                                                             class="form-control deducamount" id="taxinput"></td>
-                                                    <td style="padding:10px"><input type="checkbox" name="tax_manager"
-                                                            id="taxmanager" onchange='handletax(this);' value="1">Tax
-                                                        Manager</td>
+                                                    <td style="padding:10px">
+                                                        <input type="checkbox" name="tax_manager" id="taxmanager"
+                                                            onchange='handletax(this);' value="1">Tax
+                                                        Manager
+                                                        <select name="calculation_type[]" class="form-control calc-type"
+                                                            id="calc_type_tax"
+                                                            onchange="handleCalculationType(this, 'taxinput')"
+                                                            style="display:inline-block; width:80px; margin-left:10px;">
+                                                            <option value="0">%</option>
+                                                            <option value="1">Amount</option>
+                                                        </select>
+                                                    </td>
                                                 </tr>
 
                                             </table>
@@ -183,52 +213,94 @@
 </div>
 
 <script type="text/javascript">
-function summary() {
-    var addper = 0;
-    $(".addamount").each(function() {
-        isNaN(this.value) || 0 == this.value.length || (addper += parseFloat(this.value))
-    });
-    if (addper > 100) {
-        alert('You Can Not input more than 100%');
+// Handle calculation type change
+function handleCalculationType(selectElement, inputId) {
+    var inputElement = document.getElementById(inputId);
+    if (selectElement.value == '1') {
+        // Amount selected - remove any % validation
+        inputElement.setAttribute('data-calc-type', 'amount');
+    } else {
+        // Percentage selected
+        inputElement.setAttribute('data-calc-type', 'percentage');
     }
-    var b = parseInt($('#basic').val());
+    summary(); // Recalculate on change
+}
+
+function summary() {
+    var b = parseFloat($('#basic').val()) || 0;
     var add = 0;
     var deduct = 0;
+    var addper = 0;
+
+    // Calculate additions
     $(".addamount").each(function() {
-        var value = this.value;
-        var basic = parseInt($('#basic').val());
-        isNaN(value * basic / 100) || 0 == (value * basic / 100).length || (add += parseFloat(value * basic /
-            100))
+        var value = parseFloat(this.value) || 0;
+        if (value > 0) {
+            var calcType = $(this).closest('tr').find('.calc-type').val();
+
+            if (calcType == '0') { // Percentage
+                addper += value;
+                if (addper > 100) {
+                    alert('Total addition percentage cannot exceed 100%');
+                    this.value = '';
+                    return false;
+                }
+                add += (b * value / 100);
+            } else { // Amount
+                add += value;
+            }
+        }
     });
+
+    // Calculate deductions
     $(".deducamount").each(function() {
-        var value = this.value;
-        var basic = parseInt($('#basic').val());
-        isNaN(value * basic / 100) || 0 == (value * basic / 100).length || (deduct += parseFloat(value * basic /
-            100))
+        var value = parseFloat(this.value) || 0;
+        if (value > 0) {
+            var calcType = $(this).closest('tr').find('.calc-type').val();
+
+            if (calcType == '0') { // Percentage
+                deduct += (b * value / 100);
+            } else { // Amount
+                deduct += value;
+            }
+        }
     });
-    document.getElementById('grsalary').value = add + b - (deduct);
+
+    var grossSalary = b + add - deduct;
+    document.getElementById('grsalary').value = grossSalary.toFixed(2);
 }
 
 function handletax(checkbox) {
     var deduct = 0;
     var add = 0;
     var b = parseInt($('#basic').val());
+
     $(".deducamount").each(function() {
-        var value = this.value;
-        var basic = parseInt($('#basic').val());
-        isNaN(value * basic / 100) || 0 == (value * basic / 100).length || (deduct += parseFloat(value * basic /
-            100))
+        var value = parseFloat(this.value) || 0;
+        if (value > 0) {
+            var calcType = $(this).closest('tr').find('.calc-type').val();
+            if (calcType == '0') {
+                deduct += (b * value / 100);
+            } else {
+                deduct += value;
+            }
+        }
     });
+
     $(".addamount").each(function() {
-        var value = this.value;
-        var basic = parseInt($('#basic').val());
-        isNaN(value * basic / 100) || 0 == (value * basic / 100).length || (add += parseFloat(value * basic /
-            100))
+        var value = parseFloat(this.value) || 0;
+        if (value > 0) {
+            var calcType = $(this).closest('tr').find('.calc-type').val();
+            if (calcType == '0') {
+                add += (b * value / 100);
+            } else {
+                add += value;
+            }
+        }
     });
 
     var amount = b - deduct;
-    var tax = parseInt($('#taxinput').val());
-    var netamount = amount + tax;
+
     if (checkbox.checked == true) {
         $.ajax({
             url: '<?php echo site_url('payroll/Payroll/salarywithtax/')?>',
@@ -236,10 +308,9 @@ function handletax(checkbox) {
             dataType: 'json',
             data: {
                 'amount': amount,
-
             },
             success: function(data) {
-                document.getElementById('grsalary').value = add + b - data - deduct;
+                document.getElementById('grsalary').value = (add + b - data - deduct).toFixed(2);
                 document.getElementById('taxinput').value = '';
             },
             error: function(jqXHR, textStatus, errorThrown) {
@@ -247,27 +318,12 @@ function handletax(checkbox) {
             }
         });
     } else {
-        var b = parseInt($('#basic').val());
-        var add = 0;
-        var deduct = 0;
-        $(".addamount").each(function() {
-            var value = this.value;
-            var basic = parseInt($('#basic').val());
-            isNaN(value * basic / 100) || 0 == (value * basic / 100).length || (add += parseFloat(value *
-                basic / 100))
-        });
-        $(".deducamount").each(function() {
-            var value = this.value;
-            var basic = parseInt($('#basic').val());
-            isNaN(value * basic / 100) || 0 == (value * basic / 100).length || (deduct += parseFloat(value *
-                basic / 100))
-        });
-        document.getElementById('grsalary').value = add + b - (deduct);
+        summary();
     }
 }
-//onchange empoyee id information
+
+// Onchange employee id information
 function employechange(id) {
-    //alert(id);
     $.ajax({
         url: "<?php echo base_url('payroll/Payroll/employeebasic/')?>",
         method: 'post',
@@ -280,6 +336,7 @@ function employechange(id) {
             document.getElementById('sal_type').value = data.rate_type;
             document.getElementById('sal_type_name').value = data.stype;
             document.getElementById('grsalary').value = '';
+
             if (data.rate_type == 1) {
                 document.getElementById("taxinput").disabled = true;
                 document.getElementById("taxmanager").checked = true;
@@ -289,18 +346,28 @@ function employechange(id) {
                 document.getElementById("taxmanager").checked = false;
                 document.getElementById("taxmanager").removeAttribute('disabled');
             }
+
+            // Clear all input fields
             var i;
             var count = $('#add tr').length;
             for (i = 0; i < count; i++) {
-                document.getElementById('add_' + i).value = '';
-                //document.getElementById('dd_'+i).value='';
+                if (document.getElementById('add_' + i)) {
+                    document.getElementById('add_' + i).value = '';
+                    if (document.getElementById('calc_type_add_' + i)) {
+                        document.getElementById('calc_type_add_' + i).value = '0';
+                    }
+                }
             }
 
-            // var dt = $('#dduct tr').length;
-            // alert(dt);
-            // for (i = 0; i < dt; i++) { 
-            //    document.getElementById('dd_'+i).value='';
-            // }
+            var dt = $('#dduct tr').length;
+            for (i = 0; i < dt; i++) {
+                if (document.getElementById('dd_' + i)) {
+                    document.getElementById('dd_' + i).value = '';
+                    if (document.getElementById('calc_type_dd_' + i)) {
+                        document.getElementById('calc_type_dd_' + i).value = '0';
+                    }
+                }
+            }
         },
         error: function(jqXHR, textStatus, errorThrown) {
             alert('Error get data from ajax');
