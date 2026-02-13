6325aec6b1fcc560917996ba9157f437f1140ce1
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-02-13 17:47:14 +0530

    change in accounts.php

--- application/modules/accounts/controllers/Accounts.php
+++ application/modules/accounts/controllers/Accounts.php
@@ -445,7 +445,7 @@ public function update_debit_voucher(){
     $data['acc']        = $this->accounts_model->Transacc();
     $data['voucher_no'] = $this->accounts_model->journal();
     $content            = $this->parser->parse('accounts/journal_voucher', $data, true);
-    $this->template->full_admin_html_view($content); 
+    echo Modules::run('template/layout', $data); 
   }
 
 //    //Create Journal Voucher
@@ -520,7 +520,7 @@ public function update_debit_voucher(){
     $data['crcc']           = $this->accounts_model->Cracc();
     $data['acc']            = $this->accounts_model->Transacc();
      $content = $this->parser->parse($page, $data, true);
-    $this->template->full_admin_html_view($content); 
+    echo Modules::run('template/layout', $data); 
   }
   // update credit voucher 
   public function update_credit_voucher(){
@@ -614,7 +614,7 @@ public function update_debit_voucher(){
 
     $data['title'] = display('accounts_form');
      $content = $this->parser->parse('newaccount/vouchar_cash', $data, true);
-    $this->template->full_admin_html_view($content);
+    echo Modules::run('template/layout', $data);
     }
 // // working
     public function general_ledger(){
@@ -711,7 +711,7 @@ public function update_debit_voucher(){
       $data['software_info'] = $this->Accounts_model->software_setting_info();
       $data['title'] = display('Inventory_ledger');
       $content = $this->parser->parse('newaccount/inventory_ledger', $data, true);
-    $this->template->full_admin_html_view($content);
+    echo Modules::run('template/layout', $data);
     }
      public function voucher_report(){
         // $this->permission->method('accounts','read')->redirect();
