2d7bb41f25b410e5ccc42590cd099cc152f667f4
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-02-16 12:55:20 +0530

    change in accounts.php

--- application/modules/template/views/includes/sidebar.php
+++ application/modules/template/views/includes/sidebar.php
@@ -4,8 +4,8 @@
     <div class="user-panel text-center">
         <div class="image">
             <?php $image = $this->session->userdata('image') ?>
-            <img src="<?php echo base_url((!empty($image)?$image:'assets/img/icons/default.jpg')) ?>" class="img-circle"
-                alt="User Image">
+            <img src="<?php echo base_url((!empty($image)?$image:'assets/img/icons/mini-logo.png')) ?>"
+                class="img-circle" alt="User Image">
         </div>
         <div class="info">
             <p><?php echo $this->session->userdata('fullname') ?></p>
