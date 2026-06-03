b1a30ac92a13ba53feb25865c0ab9fdd48de5b8b
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-06-03 15:56:48 +0530

    add new model billing

--- application/modules/template/views/includes/sidebar.php
+++ application/modules/template/views/includes/sidebar.php
@@ -48,8 +48,9 @@
             }  
         }
         echo "<pre>";
-print_r(array_keys($HmvcMenu));
-exit;   
+var_dump(isset($HmvcMenu));
+print_r($HmvcMenu);
+die;
 
  
         if(isset($HmvcMenu) && $HmvcMenu!=null && sizeof($HmvcMenu) > 0)
