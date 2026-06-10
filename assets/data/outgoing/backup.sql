e001ab82cbba9afd395f58aafc565866695abf02
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-06-10 18:05:53 +0530

    changie in env

--- assets/data/outgoing/backup.sql
+++ assets/data/outgoing/backup.sql
@@ -80243,3 +80243,7 @@ SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
