e72fc066183c1353e98b555cda49699bb5f168ec
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-06-12 18:02:30 +0530

    changie in env

--- assets/data/outgoing/backup.sql
+++ assets/data/outgoing/backup.sql
@@ -80915,3 +80915,9 @@ WHERE `status` IN('unpaid', 'sent')
 AND `due_date` < '2026-06-12'
 AND `deleted_at` IS NULL;
 SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+UPDATE `cb_invoices` SET `status` = 'overdue'
+WHERE `status` IN('unpaid', 'sent')
+AND `due_date` < '2026-06-12'
+AND `deleted_at` IS NULL;
+SET SESSION sql_mode = "";
