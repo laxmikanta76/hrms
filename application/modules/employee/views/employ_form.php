97047f0736106c3dfbf37b5d628b4ea644423727
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-06-22 16:42:13 +0530

    changie in salaryslip

--- application/modules/employee/views/employ_form.php
+++ application/modules/employee/views/employ_form.php
@@ -126,6 +126,14 @@ ul li a {
                                                 placeholder="Your Zip Code">
                                         </div>
                                     </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label>PAN Number</label>
+                                            <input type="text" class="form-control" name="pan_number"
+                                                value="<?php echo isset($data->pan_number)?$data->pan_number:''; ?>"
+                                                placeholder="PAN Number">
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="form-group text-right">
                                     <input type="button" class="btn btn-primary btnNext" onclick="valid_inf()"
@@ -741,7 +749,41 @@ ul li a {
                                         </div>
                                     </div>
 
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>Bank Name</label>
+                                            <input type="text" class="form-control" name="bank_name"
+                                                value="<?php echo isset($data->bank_name)?$data->bank_name:''; ?>"
+                                                placeholder="Bank Name">
+                                        </div>
+                                    </div>
 
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>Bank Account Number</label>
+                                            <input type="text" class="form-control" name="bank_account_no"
+                                                value="<?php echo isset($data->bank_account_no)?$data->bank_account_no:''; ?>"
+                                                placeholder="Bank Account Number">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>PF Number</label>
+                                            <input type="text" class="form-control" name="pf_number"
+                                                value="<?php echo isset($data->pf_number)?$data->pf_number:''; ?>"
+                                                placeholder="PF Number">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>UAN Number</label>
+                                            <input type="text" class="form-control" name="uan_number"
+                                                value="<?php echo isset($data->uan_number)?$data->uan_number:''; ?>"
+                                                placeholder="UAN Number">
+                                        </div>
+                                    </div>
                                     <div class="col-sm-6">
                                         <label for="picture"><?php echo display('picture')?></label>
                                         <input type="file" accept="image/*" name="picture" onchange="loadFile(event)">
