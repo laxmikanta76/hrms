de5c801cade1d8bae462f5542842a1e453693c71
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-07-01 18:07:49 +0530

    changie in salaryslip

--- assets/data/outgoing/backup.sql
+++ assets/data/outgoing/backup.sql
@@ -81843,3 +81843,7 @@ SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
