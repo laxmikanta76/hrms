5be70fbbb300f6eef134326f081dcee47642371b
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-07-09 16:17:27 +0530

    adding wpa

--- assets/data/outgoing/backup.sql
+++ assets/data/outgoing/backup.sql
@@ -82048,3 +82048,16 @@ SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
 SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+UPDATE `user` SET `last_login` = '2026-07-09 14:45:08', `ip_address` = '::1'
+WHERE `id` = '1';
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
+SET SESSION sql_mode = "";
