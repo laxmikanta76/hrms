90297a15b81763ce41ac8cd85a2cdfc830ee3207
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-07-11 12:42:44 +0530

    adding wpa

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,15 @@
-const CACHE_NAME = "accrosian-hrms-v1";
+const CACHE_NAME = "accrosian-hrms-v2";
 
-const urlsToCache = [
-    "./",
-    "./manifest.json",
-    "./offline.html"
-];
-
-self.addEventListener("install", function(event) {
-
-    event.waitUntil(
-
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-
-            return cache.addAll(urlsToCache);
-
-        })
-
-    );
-
+self.addEventListener("install", (event) => {
+    console.log("Service Worker Installed");
+    self.skipWaiting();
 });
 
-self.addEventListener("activate", function(event) {
-
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker Activated");
     event.waitUntil(self.clients.claim());
-
 });
 
-self.addEventListener("fetch", function(event) {
-
-    if(event.request.method !== "GET"){
-
-        return;
-
-    }
-
-    event.respondWith(
-
-        caches.match(event.request)
-
-        .then(function(response){
-
-            return response || fetch(event.request);
-
-        })
-
-        .catch(function(){
-
-            return caches.match("./offline.html");
-
-        })
-
-    );
-
+self.addEventListener("fetch", (event) => {
+    // Let browser handle requests normally
 });
